aead63fefd5079c449308ed8b39f579985b0fe7d
sreelatha303 <sreelatha.yelesam@techolution.com>
2022-05-04 09:46:07 +0530

    db connection

--- nest-a2/src/app.module.ts
+++ nest-a2/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoController } from './todo/todo/todo.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Book, BookSchema } from './book/book/book.schema';
 import { BookService } from './book/book/book.service';
@@ -9,7 +8,7 @@ import { BookController } from './book/book/book.controller';
 @Module({
   imports: [ MongooseModule.forRoot('mongodb+srv://nest-test:SJDpU7QwxkHGgaYp@cluster0.1g5wf.mongodb.net/nestDatabase?retryWrites=true&w=majority'),
   MongooseModule.forFeature([{name: Book.name, schema: BookSchema}])],
-  controllers: [AppController, TodoController, BookController],
+  controllers: [AppController, BookController],
   providers: [AppService, BookService],
 })
 export class AppModule {}
